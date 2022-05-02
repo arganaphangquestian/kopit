5931bd8c58042ef9ec540d4fea7747b662e346c9
arganaphangquestian <arganaphangquestian@gmail.com>
2022-05-02 23:02:28 +0700

    update cors

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const axios = require("axios");
 const router = express.Router();
 
 router.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
